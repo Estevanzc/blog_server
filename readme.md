64241ee2c4d6f0ebe89cdb433488a23a700e6273
Estevanzc <estevan.zimermann@gmail.com>
2026-01-02 14:19:22 -0300

    popular page select

--- readme.md
+++ readme.md
@@ -1,63 +1,18 @@
-users = {
-    name
-    email
-    password
-    birth
-    description
-    dark_mode
-    banner
-    photo
-},
-blogs = {
-    name
-    subname
-    description
-    category_id
-},
-follower = {
-    user_id
-    blog_id
-},
-member = {
-    role
-    user_id
-    blog_id
-},
-posts = {
-    title
-    subtitle
-    summary
-    member_id
-    blog_id
-    banner
-},
-post_views = {
-    user_id
-    post_id
-},
-post_likes = {
-    user_id
-    post_id
-},
-categories = {
-    name
-},
-tags = {
-    name
-},
-post_tags = {
-    post_id
-    tag_id
-},
-preferences = {
-    keyword
-    user_id
-},
-post_contents = {
-    post_id
-    type
-    content
-    order
-},
+popular
+trending the last three days
+trending topics [likes, views, comments]
+trending last month
 
-npx sequelize-cli model:generate --name Blog --attributes name:string,subname:string,description:text,category_id:integer
+recents
+first post of the day
+first topics
+all
+
+topics
+Techonology
+Culture and Society
+Science
+Politics
+Art
+
+All
--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -6,8 +6,241 @@ const uploadConfig = require('../config/upload');
 const upload = multer(uploadConfig);
 const fs = require('fs/promises');
 const path = require('path');
+const { Op, Sequelize } = require('sequelize');
 
 module.exports = {
+  async popular(req, res, next) {
+    try {
+      let since = new Date();
+      since.setDate(since.getDate() - 30);
+
+      let trending_posts = await Post.findAll({
+        where: {
+          createdAt: {
+            [Op.gte]: since
+          }
+        },
+        attributes: {
+          include: [
+            [Sequelize.fn('COUNT', Sequelize.fn('DISTINCT', Sequelize.col('views.id'))), 'viewCount'],
+            [Sequelize.fn('COUNT', Sequelize.fn('DISTINCT', Sequelize.col('likes.id'))), 'likeCount'],
+            [Sequelize.fn('COUNT', Sequelize.fn('DISTINCT', Sequelize.col('comments.id'))), 'commentCount']
+          ]
+        },
+        include: [
+          {
+            model: View,
+            as: 'views',
+            attributes: [],
+            where: {
+              createdAt: { [Op.gte]: since }
+            },
+            required: false
+          },
+          {
+            model: Like,
+            as: 'likes',
+            attributes: [],
+            where: {
+              createdAt: { [Op.gte]: since }
+            },
+            required: false
+          },
+          {
+            model: Comment,
+            as: 'comments',
+            attributes: [],
+            where: {
+              createdAt: { [Op.gte]: since }
+            },
+            required: false
+          }
+        ],
+        group: ['Post.id'],
+        order: [[
+          Sequelize.literal(
+            '(COUNT(DISTINCT views.id) * 1 + COUNT(DISTINCT likes.id) * 3 + COUNT(DISTINCT comments.id) * 2)'
+          ),
+          'DESC'
+        ]],
+        limit: 31
+      });
+      let most_viewed = await Post.findAll({
+        where: {
+          createdAt: {
+            [Op.gte]: since
+          }
+        },
+        attributes: {
+          include: [
+            [Sequelize.fn('COUNT', Sequelize.fn('DISTINCT', Sequelize.col('views.id'))), 'viewCount'],
+          ]
+        },
+        include: [
+          {
+            model: View,
+            as: 'views',
+            attributes: [],
+            where: {
+              createdAt: { [Op.gte]: since }
+            },
+            required: false
+          },
+          {
+            model: Like,
+            as: 'likes',
+            attributes: [],
+            where: {
+              createdAt: { [Op.gte]: since }
+            },
+            required: false
+          },
+          {
+            model: Comment,
+            as: 'comments',
+            attributes: [],
+            where: {
+              createdAt: { [Op.gte]: since }
+            },
+            required: false
+          }
+        ],
+        group: ['Post.id'],
+        order: [[
+          Sequelize.literal(
+            '(COUNT(DISTINCT views.id)'
+          ),
+          'DESC'
+        ]],
+        limit: 5
+      });
+      let most_liked = await Post.findAll({
+        where: {
+          createdAt: {
+            [Op.gte]: since
+          }
+        },
+        attributes: {
+          include: [
+            [Sequelize.fn('COUNT', Sequelize.fn('DISTINCT', Sequelize.col('likes.id'))), 'likeCount'],
+          ]
+        },
+        include: [
+          {
+            model: View,
+            as: 'views',
+            attributes: [],
+            where: {
+              createdAt: { [Op.gte]: since }
+            },
+            required: false
+          },
+          {
+            model: Like,
+            as: 'likes',
+            attributes: [],
+            where: {
+              createdAt: { [Op.gte]: since }
+            },
+            required: false
+          },
+          {
+            model: Comment,
+            as: 'comments',
+            attributes: [],
+            where: {
+              createdAt: { [Op.gte]: since }
+            },
+            required: false
+          }
+        ],
+        group: ['Post.id'],
+        order: [[
+          Sequelize.literal(
+            'COUNT(DISTINCT likes.id) * 3'
+          ),
+          'DESC'
+        ]],
+        limit: 5
+      });
+      let most_commented = await Post.findAll({
+        where: {
+          createdAt: {
+            [Op.gte]: since
+          }
+        },
+        attributes: {
+          include: [
+            [Sequelize.fn('COUNT', Sequelize.fn('DISTINCT', Sequelize.col('comments.id'))), 'commentCount']
+          ]
+        },
+        include: [
+          {
+            model: View,
+            as: 'views',
+            attributes: [],
+            where: {
+              createdAt: { [Op.gte]: since }
+            },
+            required: false
+          },
+          {
+            model: Like,
+            as: 'likes',
+            attributes: [],
+            where: {
+              createdAt: { [Op.gte]: since }
+            },
+            required: false
+          },
+          {
+            model: Comment,
+            as: 'comments',
+            attributes: [],
+            where: {
+              createdAt: { [Op.gte]: since }
+            },
+            required: false
+          }
+        ],
+        group: ['Post.id'],
+        order: [[
+          Sequelize.literal(
+            'COUNT(DISTINCT comments.id) * 2'
+          ),
+          'DESC'
+        ]],
+        limit: 5
+      });
+      let trending_tags = await Tag.findAll({
+        attributes: {
+          include: [
+            [Sequelize.fn('COUNT', Sequelize.col('posts.id')), 'postCount']
+          ]
+        },
+        include: [{
+          model: Post,
+          as: 'posts',
+          attributes: [],
+          where: {
+            publishedAt: { [Op.gte]: since }
+          }
+        }],
+        group: ['Tag.id'],
+        order: [[Sequelize.literal('postCount'), 'DESC']],
+        limit: 20
+      });
+      return res.json({
+        "trending_posts": trending_posts,
+        "most_viewed": most_viewed,
+        "most_liked": most_liked,
+        "most_commented": most_commented,
+        "trending_tags": trending_tags
+      })
+    } catch (err) {
+      next(err);
+    }
+  },
   async blog_posts(req, res, next) {
     try {
       let { id } = req.params
