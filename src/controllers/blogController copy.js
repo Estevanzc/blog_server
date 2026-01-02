ed3adda899b39de231f22176cfb6421c5bc36b53
Estevanzc <estevan.zimermann@gmail.com>
2026-01-02 10:32:00 -0300

    user data select

---
+++ src/controllers/blogController copy.js
@@ -0,0 +1,41 @@
+const controller = require('../controllers/controller');
+const { Blog, Member, Follower, User } = require('../../models');
+const jwt = require('jsonwebtoken');
+const multer = require('multer');
+const uploadConfig = require('../config/upload');
+const upload = multer(uploadConfig);
+const fs = require('fs/promises');
+const path = require('path');
+
+module.exports = {
+  async blog_followers(req, res, next) {
+    try {
+      let { id } = req.params
+      let followers = await Follower.findAll({
+        where: { user_id: id },
+        include: [{
+          model: User,
+          as: 'user',
+        }]
+      });
+      return res.json(followers)
+    } catch (err) {
+      next(err);
+    }
+  },
+  async user_following(req, res, next) {
+    try {
+      let { id } = req.params
+      let following = await Follower.findAll({
+        where: { user_id: id },
+        include: [{
+          model: Blog,
+          as: 'blog',
+        }]
+      });
+      return res.json(following)
+    } catch (err) {
+      next(err);
+    }
+  },
+};
